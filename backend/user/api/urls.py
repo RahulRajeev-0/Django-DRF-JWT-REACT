d47eb2141702641aaddc5607df8d28c522aa3991
Rahul Rajeev <rahulrajeev836@gmail.com>
2024-01-04 01:37:45 +0530

    login + logout backend missed

--- backend/user/api/urls.py
+++ backend/user/api/urls.py
@@ -10,6 +10,9 @@ from rest_framework_simplejwt.views import (  # for JWT AUTHENTICATION
 urlpatterns = [
     path("", views.getAccountsRoutes.as_view(), name='accounts-routes'),
 
+    # authentication 
+    path("login/", views.LoginView.as_view(), name="user-login"),
+
     # FOR JWT AUTHENTICATION 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- backend/user/api/views.py
+++ backend/user/api/views.py
@@ -2,10 +2,12 @@
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-
-
-# --------------------------- views --------------------------------------
+from rest_framework.exceptions import AuthenticationFailed,ParseError
+from user.models import User
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken 
+from rest_framework import status
+# ---------------------------- views --------------------------------------
 class getAccountsRoutes(APIView):
     def get(self, request, format=None):
         routes = [
@@ -13,3 +15,33 @@ class getAccountsRoutes(APIView):
             'api/user/register',
         ]
         return Response(routes)
+
+class LoginView(APIView):
+    def post(self,request):
+        try:
+            email = request.data['email']
+            password = request.data['password']
+
+        except KeyError:
+            raise ParseError('All Fields Are Required')
+        
+        if not User.objects.filter(email=email).exists():
+            raise AuthenticationFailed ("Invalid Email Address")
+        
+        if not User.objects.filter(email=email, is_active=True).exists():
+            raise AuthenticationFailed('You are blocked by admin ! Please contact admin')
+
+        user = authenticate(email=email, password=password)
+        if user is None:
+            raise AuthenticationFailed("Invalid Password")
+        
+        refresh = RefreshToken.for_user(user)  # used to generate new refresh token for the user
+        refresh["first_name"] = str(user.first_name)   # custom cliam in the access token
+
+        content = {
+                     'refresh': str(refresh),
+                     'access': str(refresh.access_token),
+                     'isAdmin':user.is_superuser,
+                }
+        
+        return Response(content,status=status.HTTP_200_OK)
